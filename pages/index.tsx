e56426f86fff57d87a844afee6bca03f789f7a91
kixixixixi <kixixixixi@gmail.com>
2023-08-02 20:14:26 +0900

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next"
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <section
+        style={{
+          margin: "auto",
+          maxWidth: "48rem",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Text embedding sample{" "}
+        </h1>
+      </section>
+    </>
+  )
+}
+
+export default IndexPage
